5f9b259b412aab3630e475817e771aa675a4e18c
Stevan Bogosavljević <stevan_nk@ymail.com>
2016-01-21 18:48:54 +0100

    Added the first quote

--- Ziplines/RandomQuoteMachine/script.js
+++ Ziplines/RandomQuoteMachine/script.js
@@ -1,5 +1,21 @@
 function getNewRandomQuote (){
 
-    document.getElementById("quoteText").innerHTML="\"New quote " + Math.floor(Math.random()*100)+1 + ".\"";
+    // single quote
+    var quote = {
+        author:"Patrick Rothfuss",
+        book:"The Wise Man's Fear",
+        quote:"There are three things all wise men fear: the sea in storm, a night with no moon, and the anger of a gentle man"
+    };
+
+    //quote array
+    var quotes = [
+        {
+            author:"Patrick Rothfuss",
+            book:"The Wise Man's Fear",
+            quote:"There are three things all wise men fear: the sea in storm, a night with no moon, and the anger of a gentle man"
+        }
+    ];
+
+    document.getElementById("quoteText").innerHTML="\"New quote " + quotes[ Math.floor(Math.random()*quotes.length)].quote + ".\"";
 
 }

87f1e4645d4ba333a3b560c1a71ad36c2064b146
stevyhacker <stevyhacker@gmail.com>
2016-02-03 16:33:39 +0100

    Session length changing implemented

--- Ziplines/PomodoroClock/script.js
+++ Ziplines/PomodoroClock/script.js
@@ -1,3 +1,33 @@
-/**
- * Created by stevyhacker on 2.2.16..
- */
+function startTimer() {
+    var workTime = document.getElementById("work-length").textContent;
+    var breakTime = document.getElementById("break-length").textContent;
+
+}
+
+function minusBreak() {
+    var time = document.getElementById("break-length").textContent;
+    if (time > 1) {
+        time--;
+    }
+    document.getElementById("break-length").textContent = time;
+}
+
+function minusWork() {
+    var time = document.getElementById("work-length").textContent;
+    if (time > 1) {
+        time--;
+    }
+    document.getElementById("work-length").textContent = time;
+}
+
+function plusWork() {
+    var time = document.getElementById("work-length").textContent;
+    time++;
+    document.getElementById("work-length").textContent = time;
+}
+
+function plusBreak() {
+    var time = document.getElementById("break-length").textContent;
+    time++;
+    document.getElementById("break-length").textContent = time;
+}

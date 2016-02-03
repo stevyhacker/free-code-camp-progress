bc9bdee2398da99a28bf0ea54bf2b0c37070b66f
stevyhacker <stevyhacker@gmail.com>
2016-02-03 17:36:04 +0100

    Added setInterval for work time counter

--- Ziplines/PomodoroClock/script.js
+++ Ziplines/PomodoroClock/script.js
@@ -1,7 +1,27 @@
 function startTimer() {
+    //window.clearInterval(secondsInterval);
+    for (var i = 1; i < 99999; i++)
+        window.clearInterval(i);
+
     var workTime = document.getElementById("work-length").textContent;
     var breakTime = document.getElementById("break-length").textContent;
+    var secondsCounter = 60;
 
+    workTime--;
+    var secondsInterval = setInterval(countSeconds, 1000);
+
+    function countSeconds() {
+        secondsCounter--;
+        if (secondsCounter < 1) {
+            workTime--;
+            document.getElementById("timer").innerHTML = workTime + ":" + secondsCounter;
+            secondsCounter = 59;
+        }
+        else {
+            document.getElementById("timer").innerHTML = workTime + ":" + secondsCounter;
+        }
+
+    }
 }
 
 function minusBreak() {

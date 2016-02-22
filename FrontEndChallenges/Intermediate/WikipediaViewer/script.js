557c70016ddbb41072cfa6c40555981a4142a1e9
stevyhacker <stevyhacker@gmail.com>
2016-02-22 11:57:13 +0100

    Added code sample wikipedia api call

--- FrontEndChallenges/Intermediate/WikipediaViewer/script.js
+++ FrontEndChallenges/Intermediate/WikipediaViewer/script.js
@@ -1,7 +1,22 @@
 function wikiSearch() {
 
     var query = document.getElementById("query-field").value;
+    //alert(query);
+    //https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
 
-    alert(query);
+// Using XMLHttpRequest
+    xhr.setRequestHeader('Api-User-Agent', 'Example/1.0');
+
+// Using jQuery
+    $.ajax({
+        url: remoteUrlWithOrigin,
+        data: query,
+        dataType: 'json',
+        type: 'POST',
+        headers: {'Api-User-Agent': 'Example/1.0'},
+        success: function (data) {
+            // do something with data
+        }
+    });
 
 }

1c63161587d3ac8085ecf7bb7a67152fe9564809
stevyhacker <stevyhacker@gmail.com>
2016-02-27 16:45:57 +0100

    Added some twitch users, and function to get data by username

--- FrontEndChallenges/Intermediate/Twitch Status/script.js
+++ FrontEndChallenges/Intermediate/Twitch Status/script.js
@@ -2,3 +2,14 @@
 // $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
 //    console.log(data);
 //});
+
+
+var twichUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff"];
+
+
+function getUserData(username) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + username + '?callback=?', function (data) {
+        console.log(data);
+        return data;
+    });
+}

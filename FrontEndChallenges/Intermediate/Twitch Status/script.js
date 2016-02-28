de6e678efe74b530bd7b3a3947f816c637190c1d
stevyhacker <stevyhacker@gmail.com>
2016-02-28 11:21:18 +0100

    Fixed getting twitch user status

--- FrontEndChallenges/Intermediate/Twitch Status/script.js
+++ FrontEndChallenges/Intermediate/Twitch Status/script.js
@@ -3,38 +3,36 @@
 //    console.log(data);
 //});
 
-var twitchUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff","MedryBW"];
+var twitchUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "MedryBW"];
+var html = '';
 
 function getUserData(username) {
-    $.getJSON('https://api.twitch.tv/kraken/streams/' + username + '?callback=?' , function (data) {
+
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + username + '?callback=?', function (data) {
         console.log(data);
-        return data;
-    });
-}
-
-function getAllUserData() {
-    var html = '';
-
-    twitchUsers.forEach(function (user) {
-
-        console.log("user: " + user);
-        var data = getUserData(user);
         var status;
-        if(data.stream === null){
+        if (data.stream === null) {
             status = "offline";
         }
         else {
             status = "online";
         }
-
-
-        html+= user + ": " + status + " <br> ";
-
+        //$('#users').html(username + ": " + status + " <br> ");
+        html += username + ": " + status + " <br> ";
+        $('#users').html(html);
 
     });
 
-    $('#users').html(html);
+}
 
+function getAllUserData() {
+
+    twitchUsers.forEach(function (user) {
+
+        console.log("user: " + user);
+        getUserData(user);
+
+    });
 }
 
 $(document).ready(function () {

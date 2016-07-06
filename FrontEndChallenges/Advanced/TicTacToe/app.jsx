1eef0e69b50a7f31ff440c3243b628674b921907
Stevan Bogosavljevic <stevyhacker@gmail.com>
2016-07-07 01:50:32 +0200

    Added on click functions and improved Tic Tac Toe structure in React

--- FrontEndChallenges/Advanced/TicTacToe/app.jsx
+++ FrontEndChallenges/Advanced/TicTacToe/app.jsx
@@ -1,24 +1,58 @@
+var Tile = React.createClass({
+    render: function () {
+        return (
+            <div className="item-tile">
+                {this.props.value}
+            </div>
+        );
+    }
+});
+
+var GameTile = React.createClass({
+
+    getInitialState: function () {
+        return {
+            tile: ''
+        }
+    },
+
+    onTileClick: function (evt) {
+        this.setState({
+            tile: 'x'
+        });
+    },
+
+    render: function () {
+        return (
+            <div onClick={this.onTileClick} className="col-xs-3 grid-item">
+                <Tile value={this.state.tile}/>
+            </div>
+        );
+    }
+});
+
+
 var GameGrid = React.createClass({
 
     render: function () {
 
         return (
             <div id="game-grid" className="container grid">
-                <h1>Tic  Tac Toe Game in React</h1>
+                <h1>Tic Tac Toe Game in React</h1>
                 <div className="row">
-                    <div className="col-xs-3 grid-item"></div>
-                    <div className="col-xs-3 grid-item"></div>
-                    <div className="col-xs-3 grid-item"></div>
+                    <GameTile/>
+                    <GameTile/>
+                    <GameTile/>
                 </div>
                 <div className="row">
-                    <div className="col-xs-3 grid-item"></div>
-                    <div className="col-xs-3 grid-item"></div>
-                    <div className="col-xs-3 grid-item"></div>
+                    <GameTile/>
+                    <GameTile/>
+                    <GameTile/>
                 </div>
                 <div className="row">
-                    <div className="col-xs-3 grid-item"></div>
-                    <div className="col-xs-3 grid-item"></div>
-                    <div className="col-xs-3 grid-item"></div>
+                    <GameTile/>
+                    <GameTile/>
+                    <GameTile/>
                 </div>
             </div>
         );
